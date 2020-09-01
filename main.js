6e7310d4c850f582588dc3d62cf41cbdd0d5b7a5
HayBeeCoder <Oguntadeabass1@gmail.com>
2020-09-01 04:37:26 +0100

    Added the motion and collision functionality and the update function for the State class.

--- main.js
+++ main.js
@@ -21,6 +21,17 @@ class Level {
 Level.prototype.touches = function(pos, size, type) {
     var xStart = Math.floor(pos.x);
     var xEnd = Math.ceil(pos.x + size.x);
+    var yStart = Math.floor(pos.y);
+    var yEnd = Math.ceil(pos.y + size.y);
+
+    for (var y = yStart; y < yEnd; y++) {
+        for (var x = xStart; x < xEnd; x++) {
+            let isOutside = x < 0 || x >= this.width || y < 0 || y >= this.height;
+            let here = isOutside ? "wall" : this.rows[y][x];
+            if (here == type) return true;
+        }
+    }
+    return false;
 }
 
 class State {
@@ -37,6 +48,22 @@ class State {
     get player() {
         return this.actors.find(a => a.type == "player")
     }
+}
+
+State.prototype.update = function(time, keys) {
+    let actors = this.actors.map(actor => actor.update(time, this, keys));
+    let newState = new State(this.level, actors, this.status);
+
+    if (newState.status != "playing") return newState;
+    if (this.level.touches(player.pos, player.size, "lava")) {
+        return new State(this.level, actors, "lost");
+    }
+    for (let actor of actors) {
+        if (actor != player && overlap(actor, player)) {
+            newState = actor.collide(newState);
+        }
+    }
+    return newState;
 }
 
 class Vec {
@@ -89,6 +116,9 @@ class Lava {
 }
 
 Lava.prototype.size = new Vec(1, 1);
+Lava.prototype.collide = function(state) {
+    return new State(state.level, state.actors, "lost");
+}
 
 class Coin {
     constructor(pos, basePos, wobble) {
@@ -106,8 +136,14 @@ class Coin {
 }
 
 Coin.prototype.size = new Vec(0.6, 0.6);
+Coin.prototype.collide = function(state) {
+    let filtered = state.actors.filter(a => a != this);
+    let status = state.status;
+    if (!filtered.some(a => a.type == "coin")) status = "won");
+return new State(state.level, filtered, status);
+}
 
-// give the parts needed to create a Level instance.
+// gives the parts needed to create a Level instance.
 const levelChars = {
     ".": "empty",
     "#": "wall",
@@ -119,12 +155,10 @@ const levelChars = {
     "v": Lava
 };
 
-// let simpleLevel = new Level(simpleLevelPlan);
-
-// console.log(` ${simpleLevel.width} by ${simpleLevel.height}`)
 
 const scale = 20;
 
+// Utility functions 
 function elt(name, attrs, ...children) {
     let dom = document.createElement(name);
     for (let attr of Object.keys(attrs)) {
@@ -162,6 +196,15 @@ function drawActors(actors) {
         return rect;
     }))
 }
+
+function overlap(actor1, actor2) {
+    return actor1.pos.x + actor1.size.x > actor2.pos.x &&
+        actor1.pos.x < actor2.pos.x + actor2.size.x &&
+        actor1.pos.y + actor1.size.y > actor2.pos.y &&
+        actor1.pos.y < actor2.pos.y + actor2.size.y;
+
+}
+
 class DOMDisplay {
     constructor(parent, level) {
         console.log(level)
